49286c27ee6fd35a660dba0dc01428a8753d1875
Valtercio Santiago Ferreira Junior <juniorvaltercio2000@gmail.com>
2021-11-17 23:25:18 -0300

    Update and rename calculo.js to script.js

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 function calcular(){
     var Peso = parseFloat(document.getElementById("peso").value);
     var Altura = parseFloat(document.getElementById("altura").value);
-    //realizar o cálculo
+    var result = document.getElementById("resultado")
+    
+    if(isNaN(Peso) || isNaN(Altura)){
+      result.innerHTML = ("");
+    }
+    
+  else{
     var imc = Peso/(Altura*Altura);
     imc = imc.toFixed(2);
-    var result = document.getElementById("resultado")
     result.innerHTML = ("Seu IMC foi " + imc);
-   
+  } 
 }
