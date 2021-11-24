dd4765fe13f16fe6a8f6b246667d56c581ba1faf
Valtercio Santiago Ferreira Junior <juniorvaltercio2000@gmail.com>
2021-11-24 15:44:35 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
 function calcular(){
-    var Peso = parseFloat(document.getElementById("peso").value);
-    var Altura = parseFloat(document.getElementById("altura").value);
-    var result = document.getElementById("resultado")
-    
-    if(isNaN(Peso) || isNaN(Altura)){
-      result.innerHTML = ("");
-    }
-    
-  else{
-    var imc = Peso/(Altura*Altura);
-    imc = imc.toFixed(2);
-    result.innerHTML = ("Seu IMC foi " + imc);
-  } 
+	var peso = parseFloat(document.getElementById("peso").value)
+
+	var altura = parseFloat(document.getElementById("altura").value)
+
+	var result = document.getElementById("result")
+
+	if(isNaN(peso) || isNaN(altura)){
+		result.innerHTML = 'Preencha os campos corretamente'
+	}
+	
+	else{
+		var imc = peso/(altura*altura)
+		imc = imc.toFixed(2)
+		result.innerHTML = `Seu imc é ${imc}`
+	}
+	
+
+	
+
+
+
 }
